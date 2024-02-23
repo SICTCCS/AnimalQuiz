58572f85a335f5c0e0ddc3fa3d03ae9f324fc5ca
JonathanRBaker2007 <jonathanr.baker2007@gmail.com>
2024-02-23 09:57:24 -0600

    finished js file and started on adding h1 above the iframe

---
+++ animalScript.js
@@ -0,0 +1,29 @@
+var cat = document.getElementById("cat")
+var cow = document.getElementById("cow")
+var dog = document.getElementById("dog")
+var goat = document.getElementById("goat")
+var horse = document.getElementById("horse")
+var pig = document.getElementById("pig")
+var sheep = document.getElementById("sheep")
+
+cat.addEventListener("click",()=>{
+    document.querySelector("iframe").src ="https://scratch.mit.edu/projects/embed/958208184/"
+})
+cow.addEventListener("click",()=>{
+    document.querySelector("iframe").src ="https://scratch.mit.edu/projects/embed/885605066/"
+})
+dog.addEventListener("click",()=>{
+    document.querySelector("iframe").src ="https://scratch.mit.edu/projects/embed/956478923/"
+})
+goat.addEventListener("click",()=>{
+    document.querySelector("iframe").src ="https://scratch.mit.edu/projects/embed/958088760/"
+})
+horse.addEventListener("click",()=>{
+    document.querySelector("iframe").src ="https://scratch.mit.edu/projects/embed/958052578/"
+})
+pig.addEventListener("click",()=>{
+    document.querySelector("iframe").src ="https://scratch.mit.edu/projects/embed/958201981/"
+})
+sheep.addEventListener("click",()=>{
+    document.querySelector("iframe").src ="https://scratch.mit.edu/projects/embed/957935575/"
+})
