f30ebc1345dfc293d6470410904efc991c0ad1eb
JonathanRBaker2007 <jonathanr.baker2007@gmail.com>
2024-02-24 14:55:40 -0600

    finished header issue and added js function to change h1

--- animalScript.js
+++ animalScript.js
@@ -8,22 +8,29 @@ var sheep = document.getElementById("sheep")
 
 cat.addEventListener("click",()=>{
     document.querySelector("iframe").src ="https://scratch.mit.edu/projects/embed/958208184/"
+    document.querySelector("h1").textContent="Cat"
 })
 cow.addEventListener("click",()=>{
     document.querySelector("iframe").src ="https://scratch.mit.edu/projects/embed/885605066/"
+    document.querySelector("h1").textContent="Cow"
 })
 dog.addEventListener("click",()=>{
     document.querySelector("iframe").src ="https://scratch.mit.edu/projects/embed/956478923/"
+    document.querySelector("h1").textContent="Dog"
 })
 goat.addEventListener("click",()=>{
     document.querySelector("iframe").src ="https://scratch.mit.edu/projects/embed/958088760/"
+    document.querySelector("h1").textContent="Goat"
 })
 horse.addEventListener("click",()=>{
     document.querySelector("iframe").src ="https://scratch.mit.edu/projects/embed/958052578/"
+    document.querySelector("h1").textContent="Horse"
 })
 pig.addEventListener("click",()=>{
     document.querySelector("iframe").src ="https://scratch.mit.edu/projects/embed/958201981/"
+    document.querySelector("h1").textContent="Pig"
 })
 sheep.addEventListener("click",()=>{
     document.querySelector("iframe").src ="https://scratch.mit.edu/projects/embed/957935575/"
+    document.querySelector("h1").textContent="Sheep"
 })
