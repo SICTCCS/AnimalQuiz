8a2f7f481a283ba2cf4be0ee0485368b2c2792eb
JonathanRBaker2007 <jonathanr.baker2007@gmail.com>
2024-02-24 17:43:22 -0600

    finished dropdown menu, need to figure why the js file isn't running in the index. Also figure out how to pass the information through to the animals file so it automatically has the information for the iframe and h1.

--- animalScript.js
+++ animalScript.js
@@ -5,32 +5,44 @@ var goat = document.getElementById("goat")
 var horse = document.getElementById("horse")
 var pig = document.getElementById("pig")
 var sheep = document.getElementById("sheep")
+var animal = ""
+
+
 
 cat.addEventListener("click",()=>{
     document.querySelector("iframe").src ="https://scratch.mit.edu/projects/embed/958208184/"
     document.querySelector("h1").textContent="Cat"
+    animal="cat"
 })
 cow.addEventListener("click",()=>{
     document.querySelector("iframe").src ="https://scratch.mit.edu/projects/embed/885605066/"
     document.querySelector("h1").textContent="Cow"
+    animal="cow"
 })
 dog.addEventListener("click",()=>{
     document.querySelector("iframe").src ="https://scratch.mit.edu/projects/embed/956478923/"
     document.querySelector("h1").textContent="Dog"
+    animal="dog"
 })
 goat.addEventListener("click",()=>{
     document.querySelector("iframe").src ="https://scratch.mit.edu/projects/embed/958088760/"
     document.querySelector("h1").textContent="Goat"
+    animal="goat"
 })
 horse.addEventListener("click",()=>{
     document.querySelector("iframe").src ="https://scratch.mit.edu/projects/embed/958052578/"
     document.querySelector("h1").textContent="Horse"
+    animal="horse"
 })
 pig.addEventListener("click",()=>{
     document.querySelector("iframe").src ="https://scratch.mit.edu/projects/embed/958201981/"
     document.querySelector("h1").textContent="Pig"
+    animal="pig"
 })
 sheep.addEventListener("click",()=>{
     document.querySelector("iframe").src ="https://scratch.mit.edu/projects/embed/957935575/"
     document.querySelector("h1").textContent="Sheep"
+    animal="sheep"
+    alert(animal)
 })
+
